f271abb364256759148d72a5ca46f643fef69a36
Juliana Rousseau <juliana.salafia1@gmail.com>
2025-02-09 14:14:15 -0300

    feat: configs

---
+++ bin/algorithms_2_0.dart
@@ -0,0 +1,5 @@
+import 'package:algorithms_2_0/algorithms_2_0.dart' as algorithms_2_0;
+
+void main(List<String> arguments) {
+  print('Hello world: ${algorithms_2_0.calculate()}!');
+}
---
+++ test/algorithms_2_0_test.dart
@@ -0,0 +1,8 @@
+import 'package:algorithms_2_0/algorithms_2_0.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
